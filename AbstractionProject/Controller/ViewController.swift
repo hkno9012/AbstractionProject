af786a542b3b08e3c69bf05da08461d01522f6d2
HunterKnott <hunterknott00@gmail.com>
2018-12-04 12:24:55 -0700

    Initial Commit

---
+++ AbstractionProject/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbstractionProject
+//
+//  Created by Knott, Hunter on 12/4/18.
+//  Copyright © 2018 Knott, Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
